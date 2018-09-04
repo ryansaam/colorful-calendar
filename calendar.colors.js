fab2a4ee6eb636e210b2c45951b93552d0cee32c
Ryan Sam <ryansam.inquiries@gmail.com>
2018-09-03 20:40:51 -0500

    init for github

---
+++ calendar.colors.js
@@ -0,0 +1,27 @@
+const original = {
+  textColor: "#000",
+  componentBG: "#292828",
+  header1BG: "#fff",
+  header2BG: "#fff",
+  arrowsBG: "#fff",
+  weekDayNamesBG: "#fff",
+  prevMonthNodesBG: "#8ee5ff",
+  currentDateNodeBG: "#c4c1c1",
+  currentMonthNodesBG: "#fff",
+  nextMonthNodesBG: "#8ee5ff"
+}
+
+const redflat = {
+  textColor: "#fff",
+  componentBG: "#383737",
+  header1BG: "#383737",
+  header2BG: "#383737",
+  arrowsBG: "#ff2b2b",
+  weekDayNamesBG: "#c4c1c1",
+  prevMonthNodesBG: "#ff2b2b",
+  currentDateNodeBG: "#747272",
+  currentMonthNodesBG: "#c4c1c1",
+  nextMonthNodesBG: "#ff2b2b"
+}
+
+export { original, redflat }
